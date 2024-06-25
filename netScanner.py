29204bc2350813cc8307f34c8fe454e65a868a48
Büşra Yıldız <402515@ogr.ktu.edu.tr>
2024-06-25 10:17:31 -0400

    Add files via upload

---
+++ netScanner.py
@@ -0,0 +1,21 @@
+import scapy.all as scapy
+import optparse
+
+def getUserInput():
+    parseObject = optparse.OptionParser()
+    parseObject.add_option("-i", "--ip", dest="ipAddress", help="IP address to scan")
+    return parseObject.parse_args()
+
+def scanNetwork(ipAddress):
+
+    arpRequestPacket = scapy.ARP(pdst=ipAddress)
+    broadcastPacket = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    combined = arpRequestPacket/broadcastPacket
+    (answered, unanswered) = scapy.srp(combined, timeout=1)
+    answered.summary()
+
+(userInput, arguments) = getUserInput()
+if not userInput:
+    print("Enter IP address")
+scanNetwork(userInput.ipAddress)
+
